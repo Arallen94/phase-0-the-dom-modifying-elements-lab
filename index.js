0cd616ad58a041f1cd06485d795d10e1cb4407b7
Blairallen <arallen94@hotmail.com>
2024-03-25 12:40:15 -0700

    creating and inserting DOM nodes lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+let yourName = "Allen"; // Replace "Ada" with your actual name
+newHeader.textContent = `${yourName} is the champion`;
